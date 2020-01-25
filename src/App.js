d87cc78133d8a4ff5561c1bd197dc8f9413a7f2d
Wesley Sabino Dos Santos <wesley.sabino@dft-sp-wkn1124.dafiti.local>
2020-01-25 01:33:06 -0300

    feact/adding themes file

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import theme from './themes/theme'
 
 function App () {
   return (
     <>
       <h1>Pocketknives</h1>
+      <h2>{theme.colors.bg.main}</h2>
       <h2>List of desirable functionalities:</h2>
       <ul>
         <li>Healthy tools</li>
---
+++ src/themes/theme.js
@@ -0,0 +1,8 @@
+export default {
+  font: 'arial',
+  colors: {
+    bg: {
+      main: '#000'
+    }
+  }
+}
