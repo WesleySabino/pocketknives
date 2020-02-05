8e5076a72f2e351ef8a8b409dcab31567aef62c1
wesleysabino <wesleysabino@hotmail.com>
2020-02-04 23:30:55 -0300

    first semantic content

--- src/App.js
+++ src/App.js
@@ -1,21 +1,35 @@
 import React from 'react'
-import theme from './themes/theme'
+// TODO to use theme provider
+// import theme from './themes/theme'
 
 function App () {
   return (
     <>
-      <h1>Pocketknives</h1>
-      <h2>{theme.colors.bg.main} + testing</h2>
-      <h2>List of desirable functionalities:</h2>
-      <ul>
-        <li>Healthy tools</li>
-        <li>Assets manager</li>
-        <li>Articles and tutorials</li>
-        <li>Links for others tools</li>
-        <li>Fair Ux and UI</li>
-        <li>Design system</li>
-        <li>Data persistence on the server</li>
-      </ul>
+      <header>
+        <section>
+          <ul>
+            <li><a href="#">Healthy tools</a></li>
+            <li><a href="#">Assets manager</a></li>
+            <li><a href="#">Articles and tutorials</a></li>
+            <li><a href="#">Links for others tools</a></li>
+          </ul>
+        </section>
+        <section>
+          <ul>
+            <li><a href="#">Sign in</a></li>
+            <li><a href="#">Log in</a></li>
+          </ul>
+        </section>
+      </header>
+      <main>
+        <article>
+        <h1>Pocketknives</h1>
+          <h2>Purpose</h2>
+          <p>This website is meant to be a hub to solve your daily problems.</p>
+          <p>Ok, let me be honest, it is just a excuse to experiment some ideas about web development. So if you want to experiment your ideas as well you are welcome to contribute on <a href="https://github.com/WesleySabino/pocketknives">github</a>.</p>
+          <p>Enjoy it!</p>
+        </article>
+      </main>
     </>
   )
 }
