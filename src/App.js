9021b65a8a28bc87e79041e98a3a912d3b54b04b
wesleysabino <wesleysabino@hotmail.com>
2020-02-05 20:18:32 -0300

    feat: using nav

--- src/App.js
+++ src/App.js
@@ -6,30 +6,31 @@ function App () {
   return (
     <>
       <header>
-        <section>
+        <h1>Pocketknives</h1>
+      </header>
+      <main>
+        <article>
+          <h2>Purpose of This Page</h2>
+          <p>This website is meant to be a hub to solve your daily problems.</p>
+          <p>Ok, let me be honest, it is just a excuse to experiment some ideas about web development. So if you want to experiment your ideas as well you are welcome to contribute on <a href="https://github.com/WesleySabino/pocketknives">github</a>.</p>
+          <p>Enjoy it!</p>
+        </article>
+      </main>
+      <nav>
           <ul>
             <li><a href="#">Healthy tools</a></li>
             <li><a href="#">Assets manager</a></li>
             <li><a href="#">Articles and tutorials</a></li>
             <li><a href="#">Links for others tools</a></li>
           </ul>
-        </section>
-        <section>
           <ul>
             <li><a href="#">Sign in</a></li>
             <li><a href="#">Log in</a></li>
           </ul>
-        </section>
-      </header>
-      <main>
-        <article>
-        <h1>Pocketknives</h1>
-          <h2>Purpose</h2>
-          <p>This website is meant to be a hub to solve your daily problems.</p>
-          <p>Ok, let me be honest, it is just a excuse to experiment some ideas about web development. So if you want to experiment your ideas as well you are welcome to contribute on <a href="https://github.com/WesleySabino/pocketknives">github</a>.</p>
-          <p>Enjoy it!</p>
-        </article>
-      </main>
+        </nav>
+      <footer>
+        <a href="#">Footer</a>
+      </footer>
     </>
   )
 }
